762409b87844c00beeb863edaa8ca04f18b4412f
ganda <andrian.gandawida@tokopedia.com>
2022-03-09 23:34:29 +0700

    update example main

--- examples/empty-app/main.go
+++ examples/empty-app/main.go
@@ -8,7 +8,16 @@ import (
 
 func main() {
 
-	err := gank.CreateAndRunApp(gank.DefaultLifecycler(), gank.WithDefaultProviders())
+	// main function to create your application
+	err := gank.CreateAndRunApp(
+
+		// lifecycler object to handle start, stop, and dependencies injection (must use)
+		gank.DefaultLifecycler(),
+
+		// inject all default services such as : logger, http server, router, middlewares, token, etc
+		// those services will not be called if not used
+		gank.WithDefaultInternalProviders(),
+	)
 	if err != nil {
 		log.Fatal(err)
 	}
--- examples/health-app/main.go
+++ examples/health-app/main.go
@@ -8,9 +8,17 @@ import (
 
 func main() {
 
+	// main function to create your application
 	err := gank.CreateAndRunApp(
+
+		// lifecycler object to handle start, stop, and dependencies injection (must use)
 		gank.DefaultLifecycler(),
+
+		// inject all default services such as : logger, http server, router, middlewares, token, etc
+		// those services will not be called if not used
 		gank.WithDefaultInternalProviders(),
+
+		// use this option to start http health service
 		gank.WithHealthHandler(),
 	)
 	if err != nil {
