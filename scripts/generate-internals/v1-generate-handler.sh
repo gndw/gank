51c03ae0eaec3386e4a796859ad7aef9788aa3ee
ganda <andrian.gandawida@tokopedia.com>
2022-03-12 18:32:32 +0700

    remove generate scripts

--- scripts/generate-internals/v1-generate-handler.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-PACKAGE_FOLDER=$1
-PACKAGE_NAME=$2
-
-cd "$(dirname "$0")"
-
-sh generate.sh v1/internals/handlers/$PACKAGE_FOLDER $PACKAGE_NAME Handler h handler
--- scripts/generate-internals/v1-generate-usecase.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-PACKAGE_FOLDER=$1
-PACKAGE_NAME=$2
-
-cd "$(dirname "$0")"
-
-sh generate.sh v1/internals/usecases/$PACKAGE_FOLDER $PACKAGE_NAME Usecase u usecase
